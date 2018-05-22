e9a4b0587543057a69237d7139043b030f0b17a1
Иванов Алексей Анатольевич <ip711s09@mwp05.csc.local>
2018-05-22 13:00:03 +0700

    Trying to fix tests, adding return 0

--- src/chess.c
+++ src/chess.c
@@ -2,28 +2,30 @@
 #include "head.h"
 
 char a[8][8]; 
-int main() { 
-int x1,y1,x2,y2,trigger=0;
+int main() 
+	{ 
+	int x1,y1,x2,y2,trigger=0;
 
-init();
-visual();
+	init();
+	visual();
 
-printf("\nenter x1 y1:\n");
-scanf("%d %d",&x1,&y1);
-printf("\nenter x2 y2:\n");
-scanf("%d %d",&x2,&y2);
+	printf("\nenter x1 y1:\n");
+	scanf("%d %d",&x1,&y1);
+	printf("\nenter x2 y2:\n");
+	scanf("%d %d",&x2,&y2);
 
 
-do
-{
-  if ((a[8-y2][x2-1]=='k') ||( a[8-y2][x2-1]=='K')) trigger=1;  
-a[8-y2][x2-1]=a[8-y1][x1-1];
-a[8-y1][x1-1]=' ';
-visual();
- if (trigger) break;
-printf("\nenter x1 y1:\n");
-scanf("%d %d",&x1,&y1);
-printf("\nenter x2 y2:\n");
-scanf("%d %d",&x2,&y2);
- } while(1);
-} 
+	do
+	{
+ 		if ((a[8-y2][x2-1]=='k') ||( a[8-y2][x2-1]=='K')) trigger=1;  
+		a[8-y2][x2-1]=a[8-y1][x1-1];
+		a[8-y1][x1-1]=' ';
+		visual();
+ 		if (trigger) break;
+		printf("\nenter x1 y1:\n");
+		scanf("%d %d",&x1,&y1);
+		printf("\nenter x2 y2:\n");
+		scanf("%d %d",&x2,&y2);
+ 	} while(1);
+	return 0;
+	} 
